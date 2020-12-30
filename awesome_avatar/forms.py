a327eb1298386ea425a8afe694d0835fa76005a8
Piotr Antosz <boquete37@gmail.com>
2020-12-30 15:40:04 +0100

    Update forms.py

--- awesome_avatar/forms.py
+++ awesome_avatar/forms.py
@@ -6,14 +6,3 @@ from awesome_avatar.widgets import AvatarWidget
 
 class AvatarField(forms.ImageField):
     widget = AvatarWidget
-
-    def __init__(self, **defaults):
-        self.width = defaults.pop('width', config.width)
-        self.height = defaults.pop('height', config.height)
-        super(AvatarField, self).__init__(**defaults)
-
-    def widget_attrs(self, widget):
-        attrs = super().widget_attrs(widget)
-        attrs['width'] = self.width
-        attrs['height'] = self.height
-        return attrs
